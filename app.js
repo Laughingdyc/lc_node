403b85cccc9d8fd0bdd378c446087943aa93db19
My Name Is DuYuchen <duyuchen6324333@qq.com>
2022-04-15 02:11:44 +0800

    feat: /homepageViews

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const dayjs = require('dayjs')
 const mysql = require('mysql')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send(
@@ -18,23 +22,77 @@ function connect () {
   })
 }
 
+function errorResJson(status, error) {
+  return { status: status, msg: error }
+}
+function successResJson(data = []) {
+  return { status: 0, data: data }
+}
+
 app.get('/schoolList', (req, res) => {
   const conn = connect()
   conn.query('select * from lc_school', (err, results) => {
     if (err) {
-      res.json({
-        status: 1,
-        msg: err
-      })
+      res.json(errorResJson(1, err))
     } else {
-      res.json({
-        status: 0,
-        data: results
-      })
+      res.json(successResJson(results))
     }
     conn.end()
   })
 })
+
+/**
+ * @param startDate 'YYYY-MM-DD'
+ * @param endDate 'YYYY-MM-DD'
+ * @description startDate <= endDate
+ */
+app.get('/homepageViews', (req, res) => {
+  let startDate = ''
+  let endDate   = ''
+  if ( !homepageViews_params_isValid(req) ) { return }
+  startDate = req.query.startDate
+  endDate   = req.query.endDate
+  let currentDate = startDate
+  let selectCondition = ''
+  const conn = connect()
+  while ( !dayjs(currentDate).isAfter(endDate) ) {
+    if ( currentDate === startDate ) {
+      selectCondition += `home_views_date = '${currentDate}' `
+    } else {
+      selectCondition += `OR home_views_date = '${currentDate}'`
+    }
+    currentDate = dayjs(currentDate).add(1, 'day').format('YYYY-MM-DD')
+  }
+  conn.query(
+    `SELECT * FROM lc_home_views WHERE ${selectCondition}`,
+    (err, results) => {
+      if (err) {} 
+      else {
+        const _res = []
+        results.map((x) => {
+          _res.push({
+            date : dayjs(x.home_views_date).format('YYYY-MM-DD'),
+            views: x.home_views_amount
+          })
+        })
+        res.json(successResJson(_res))
+      }
+      conn.end()
+    }
+  )
+})
+
+function homepageViews_params_isValid (req) {
+  if ( !req.query.startDate || !req.query.endDate ) {
+    res.json(errorResJson(1, 'startDate 和 endDate 必须有效'))
+    return false
+  } else if ( dayjs(req.query.startDate).isAfter(dayjs(req.query.endDate)) ) {
+    res.json(errorResJson(1, '开始时间必须早于结束时间'))
+    return false
+  } else {
+    return true
+  }
+}
 
 const server = app.listen(3000, () => {
   const host = server.address().address
